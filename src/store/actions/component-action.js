75cfaa24d3b16f7ccccfce634c26d3a458a1fde0
subha <subha.maharajan@fincuro.in>
2022-12-22 19:14:28 +0530

    merge issue

--- src/store/actions/component-action.js
+++ src/store/actions/component-action.js
@@ -136,34 +136,30 @@ export const setBudgetData = (token,dateValue = {}) => {
 };
 
 export const setExpenseData = (token,dateValue = {}) => {
-  let param = {  "transactionFilter": {
-    "periodFrom": "2022-01-01",
-    "periodTo": "2022-12-31",
-    "hideExcluded": true
-},
-"options": {
-    "timeResolution": "Month",
-    "overTime": true,
-    "type": "yearSeries",
-    "includeCarbonFootprint": true
-},
-"seriesSelectors": [
-    {
-        "filter": {
-            "categoryIds": [
-                37
-            ]
-        }
-    },
-    {
-        "filter": {
-            "merchantIds": [
-                36
-            ],
-            "useParentMerchantIds": true
-        }
-    }
-] }
+  let param = {
+    "transactionFilter": {
+      "categoryIds": [-13,-32,-40,-48,-54,-64,-70,-86,-96,-112,-116,-307],
+      "periodFrom": dateValue.periodFrom,
+      "periodTo": dateValue.periodTo,
+      "hideExcluded": true
+  },
+  "options": {
+      "overTime": false
+  },
+  "seriesSelectors": [
+      {"filter": {"categoryIds": [-13]}},
+      {"filter": {"categoryIds": [-32]}},
+      {"filter": {"categoryIds": [-40]}},
+      {"filter": {"categoryIds": [-48]}},
+      {"filter": {"categoryIds": [-54]}},
+      {"filter": {"categoryIds": [-64]}},
+      {"filter": {"categoryIds": [-70]}},
+      {"filter": {"categoryIds": [-86]}},
+      {"filter": {"categoryIds": [-96]}},
+      {"filter": {"categoryIds": [-112]}},
+      {"filter": {"categoryIds": [-116]}},
+      {"filter": {"categoryIds": [-307]}},
+ ] }
   return (dispatch) => {
     axios.post(`${API_URL}/transactions/series?token=Bearer ${token}`,param).then((response) => {
       if (response.status === 200) {
